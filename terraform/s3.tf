e50007abbd6fe77a55ae44fecc58b2ab07ea7bb0
Dasari Chandana <chandanadasari34@gmail.com>
2026-03-02 22:22:16 +0530

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "chandhu24.monobucket"
+  bucket = "chandhu114.monobucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -26,7 +26,7 @@ status = "Enabled"
 terraform {
 backend "s3" {
 region = "us-east-1"
-bucket = "chandhu24.monobucket"
+bucket = "chandhu114.monobucket"
 key = "prod/terraform.tfstate"
 }
 }
