c30d5195febf4f2be9adff14a895de3bc6c05930
Dasari Chandana <chandanadasari34@gmail.com>
2026-03-03 07:58:22 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,39 @@
+resource "aws_launch_template" "web_server_as" {
+    name = "myproject"
+    image_id           = "ami-056335ec4a8783947"
+    vpc_security_group_ids = [aws_security_group.web_server.id]
+    instance_type = "t3.micro"
+    key_name = "gitkp"
+    tags = {
+        Name = "DevOps"
+    }
+}
+
+resource "aws_elb" "web_server_lb"{
+     name = "web-server-lb"
+     security_groups = [aws_security_group.web_server.id]
+     subnets = ["subnet-0257c42486a350228", "subnet-021bb955b2fd36b47"]
+     listener {
+      instance_port     = 8000
+      instance_protocol = "http"
+      lb_port           = 80
+      lb_protocol       = "http"
+    }
+    tags = {
+      Name = "terraform-elb"
+    }
+  }
+
+resource "aws_autoscaling_group" "web_server_asg" {
+    name                 = "web-server-asg"
+    min_size             = 1
+    max_size             = 3
+    desired_capacity     = 2
+    health_check_type    = "EC2"
+    load_balancers       = [aws_elb.web_server_lb.name]
+    availability_zones    = ["eu-north-1a", "eu-north-1b"] 
+    launch_template {
+        id      = aws_launch_template.web_server_as.id
+        version = "$Latest"
+      }
+}
